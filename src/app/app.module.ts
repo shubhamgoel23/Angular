aab9c5b00c1c56dad356d96ac2eec5a8f4ff8975
shubhamgoel23 <shubhamgoyal23@yahoo.com>
2017-08-20 23:23:03 +0530

    my first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { AlertModule } from 'ngx-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ProductFilterPipe } from './products/product-filter.pipe';
+import { StarComponent } from './shared/star.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductFilterPipe,
+    StarComponent
+  ],
+  imports: [
+    AlertModule.forRoot(),
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
