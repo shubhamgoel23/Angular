f24455740da1f8890884d3d543471218b3b74aca
shubhamgoel23 <shubhamgoyal23@yahoo.com>
2017-08-26 04:00:12 +0530

    modified product details component

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from "./product.service";
 
 @Component({
     moduleId: module.id,
@@ -9,17 +10,26 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent {
     pageTitle: string = 'Product Detail';
+    errorMessage: string;
     product : IProduct;
 
     constructor(private _route: ActivatedRoute,
-                    private _router: Router){
+                    private _router: Router,
+                    private _productService: ProductService){
 
     }
 
     ngOnInit():void{
         let id = +this._route.snapshot.params['id'];
-        this.pageTitle += `:${id}`;
+        //this.pageTitle += `:${id}`;
+        this.getProduct(id);
     }
+
+    getProduct(id: number) {
+        this._productService.getProduct(id).subscribe(
+          product => this.product = product,
+          error => this.errorMessage = <any>error);
+      }
 
     onBack():void{
         this._router.navigate(['/products']);
