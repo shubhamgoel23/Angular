f4326fe9bb87a94c00da684e2524af066e45ec0f
shubhamgoel23 <shubhamgoyal23@yahoo.com>
2017-08-25 17:50:04 +0530

    routes  with guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailGuard } from './products/product-guard.service';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
@@ -29,13 +30,15 @@ import { StarComponent } from './shared/star.component';
     HttpClientModule,
     RouterModule.forRoot([
       {path: 'products', component : ProductListComponent },
-      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'products/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent},
       {path: 'welcome', component : WelcomeComponent},
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
     ])
   ],
-  providers: [],
+  providers: [ProductDetailGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
